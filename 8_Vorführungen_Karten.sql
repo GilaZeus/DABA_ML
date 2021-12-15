293702834e0e8c778fd3537d5c5f52d1cddde1ae
GilaZeus <gilyazevr@gmail.com>
2021-12-15 11:01:23 +0100

    Create 8_Vorführungen_Karten.sql

---
+++ 8_Vorführungen_Karten.sql
@@ -0,0 +1,9 @@
+-- Zuerst die Kundengruppen:
+INSERT INTO Kundengruppe (Bezeichnung) VALUES ('Kind');
+INSERT INTO Kundengruppe (Bezeichnung) VALUES ('Student');
+INSERT INTO Kundengruppe (Bezeichnung) VALUES ('Erwachsener');
+INSERT INTO Kundengruppe (Bezeichnung) VALUES ('Behinderte');
+
+COMMIT;
+
+-- Vorführungen für Le Professional bei KuKubi:
